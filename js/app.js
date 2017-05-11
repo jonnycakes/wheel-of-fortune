8712070e00602b2ab4b193d58ededd5342d339a9
John <johnfarrelly@gmail.com>
2017-05-11 12:06:46 -0400

    saving progress in set players name

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
   console.log("Hello");
 
-  // Useful Alisas
+  // Call for the bootstrap modal on page load. h/t http://stackoverflow.com/questions/10233550/launch-bootstrap-modal-on-page-load
+  $('#myModal').modal('show')
+
+
   var $player1NameText = $("#player-1-name");
   var $player1NameDisplay = $("#player-1-name-display");
   var $player2NameText = $("#player-2-name");
@@ -10,7 +13,7 @@ $(document).ready(function() {
   var $player3NameDisplay = $("#player-3-name-display");
   var $submitButton = $("#submit-player-names")
 
-  // Submit Player Names
+  // Submit Player Names to be displayed
   $submitButton.on("click", function() {
     $player1NameDisplay.text(`Name: ${$player1NameText.val()}`)
     $player2NameDisplay.text(`Name: ${$player2NameText.val()}`)
