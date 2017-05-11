1990a4cc548ac8485bc74a171ec960a3f4d532d5
John <johnfarrelly@gmail.com>
2017-05-11 14:12:34 -0400

    Added Turn tracking concept

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   // Call for the bootstrap modal on page load. h/t http://stackoverflow.com/questions/10233550/launch-bootstrap-modal-on-page-load
   $('#myModal').modal('show')
 
-
+  // aliases!
   var $player1NameText = $("#player-1-name");
   var $player1NameDisplay = $("#player-1-name-display");
   var $player2NameText = $("#player-2-name");
@@ -13,10 +13,13 @@ $(document).ready(function() {
   var $player3NameDisplay = $("#player-3-name-display");
   var $submitButton = $("#submit-player-names")
 
-
-
+  // Userful Variables
   var words = ["HelloWorld", "Disambiguation", "General", "Assembly"];
   var letters;
+  var player1Score = 0;
+  var player2Score = 0;
+  var player3Score = 0;
+  var turn = "player1"
 
   // Submit Player Names to be displayed
   $submitButton.on("click", function() {
@@ -25,20 +28,42 @@ $(document).ready(function() {
     $player3NameDisplay.text(`Name: ${$player3NameText.val()}`)
   })
 
+  // **************************
+  // **************************
+  // **************************
+
   // Program Pseudocode - Choose a random word
-  // Choose a word from the words away randomly
-  // break down the word into each letter
-  // display each letter as an underscore
-  // Using https://jsfiddle.net/phollott/x29ym2ag/ to guide me through, but I'm trying to convert as much of it as I can to jQuery
+  // // CHOOSE a word from the words away randomly
+  // // SEPERATE each letter in the word
+  // // DISPLAY each letter as an empty box
+  // Using https://jsfiddle.net/phollott/x29ym2ag/ as inspitation here, for logic, but styling and how we "split" the puzzle into letters is much different for me.
 
   var puzzle = words[Math.floor(Math.random() * words.length)]
   var letters = puzzle.toUpperCase().split("")
   letters.forEach(function(letter) {
-      $("#puzzle-wrapper ul").append(
-        `<li><span class="start">${letter}</span></li>`)
-    })
-    // puzzle.charAt(index).forEach(function(letter) {
-    //     $("puzzle-wrapper ul").append(`<li>${letter}</li>")
-    //   })
+    $("#puzzle-wrapper ul").append(
+      `<li><span class="start">${letter}</span></li>`)
+  })
 
+  // **************************
+  // **************************
+  // **************************
+
+  // Increment the turn
+  var incrementTurn = function() {
+    if (turn === "player1") {
+      turn = "player2";
+      $("#player-1").removeClass("player-1-turn")
+      $("#player-2").addClass("player-2-turn")
+    } else if (turn === "player2") {
+      turn = "player3";
+      $("#player-2").removeClass("player-2-turn")
+      $("#player-3").addClass("player-3-turn")
+    } else {
+      turn = "player1"
+      $("#player-3").removeClass("player-3-turn")
+      $("#player-1").addClass("player-1-turn")
+    }
+  }
+  $("#increment").on("click", incrementTurn)
 })
