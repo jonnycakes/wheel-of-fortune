dfe0ce346f851f8f2c760f2c55207b8982824980
John <johnfarrelly@gmail.com>
2017-05-11 10:17:24 -0400

    Players submit names

--- js/app.js
+++ js/app.js
@@ -1,7 +1,21 @@
 $(document).ready(function() {
-  console.log("Hello")
+  console.log("Hello");
 
   // Useful Alisas
-  $player1NameText =
-    $player1NameDisplay =
+  var $player1NameText = $("#player-1-name");
+  var $player1NameDisplay = $("#player-1-name-display");
+  var $player2NameText = $("#player-2-name");
+  var $player2NameDisplay = $("#player-2-name-display");
+  var $player3NameText = $("#player-3-name");
+  var $player3NameDisplay = $("#player-3-name-display");
+  var $submitButton = $("#submit-player-names")
+
+  // Submit Player Names
+  $submitButton.on("click", function() {
+    $player1NameDisplay.text(`Name: ${$player1NameText.val()}`)
+    $player2NameDisplay.text(`Name: ${$player2NameText.val()}`)
+    $player3NameDisplay.text(`Name: ${$player3NameText.val()}`)
+  })
+
+
 })
